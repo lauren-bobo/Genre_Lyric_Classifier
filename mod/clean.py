3ed6a111a5cfc8be4277abc7c7ef11df4ff312dc
lauren-bobo <ldb44912@uga.edu>
2024-12-06 16:40:20 -0500

    db

---
+++ mod/clean.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
