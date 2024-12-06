88b64f91b6e3d071a585323877e54a695aae5962
lauren-bobo <ldb44912@uga.edu>
2024-12-06 16:48:19 -0500

    loaded data

--- mod/clean.py
+++ mod/clean.py
@@ -6,3 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 
+Path_To_Test_Data = "C:/Users/ldbob/Downloads/Music_Genre_Classifier/Music_Lyric_DB/DB/cleaned_test_lyrics.csv"
+Path_To_Train_data = "C:/Users/ldbob/Downloads/Music_Genre_Classifier/Music_Lyric_DB/DB/cleaned_train_lyrics.csv"
+
+def load_data():
+    train_data = pd.read_csv(Path_To_Train_data)
+    test_data = pd.read_csv(Path_To_Test_Data)
+    return train_data, test_data
+
+
+load_data()
+
